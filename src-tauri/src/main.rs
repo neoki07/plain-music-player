caa4f971b11955a189fcee46d9a88de2cebb1a87
ot07 <ot07neoki@gmail.com>
2022-06-06 02:21:53 +0900

    :construction: Use termusic source code instead of rodio as player

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,27 +3,46 @@
     windows_subsystem = "windows"
 )]
 
-use rodio::{source::Source, Decoder, OutputStream};
+mod player;
+
 use std::fs::File;
 use std::io::BufReader;
+use std::path::Path;
+use std::thread::sleep;
+use std::time::Duration;
 use tauri::Manager;
+use player::Player;
 
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
             let id = app.listen_global("front-to-back", |event| {
-                // Get a output stream handle to the default physical sound device
-                let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-                // Load a sound from a file, using a path relative to Cargo.toml
-                let file = BufReader::new(File::open(event.payload().unwrap()).unwrap());
-                // Decode that sound file into a source
-                let source = Decoder::new(file).unwrap();
-                // Play the sound directly on the device
-                stream_handle.play_raw(source.convert_samples());
+                let mut player = Player::new();
+                println!("event.payload: {:?}", event.payload().unwrap());
+                player.play(Path::new(event.payload().unwrap()));
 
-                // The sound plays in a separate audio thread,
-                // so we need to keep the main thread alive while it's playing.
-                std::thread::sleep(std::time::Duration::from_secs(30));
+                let mut seconds = 0;
+
+                for i in 0..10 {
+                    println!("{} elapsed: {:?}", i, player.get_progress().unwrap());
+                    sleep(Duration::new(0, 123456789));
+                }
+
+                let _ = player.seek_to(Duration::new(50, 0));
+                println!("\n=============== seek ===============\n");
+
+                for i in 0..10 {
+                    println!("{} elapsed: {:?}", i, player.get_progress().unwrap());
+                    sleep(Duration::new(0, 123456789));
+                }
+
+                let _ = player.seek_to(Duration::new(10, 0));
+                println!("\n=============== seek ===============\n");
+
+                for i in 0..10 {
+                    println!("{} elapsed: {:?}", i, player.get_progress().unwrap());
+                    sleep(Duration::new(0, 123456789));
+                }
             });
             Ok(())
         })

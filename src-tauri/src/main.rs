471d1d7f3d320399c25f3dee4a7c56fe4375c19e
ot07 <ot07neoki@gmail.com>
2022-06-12 00:28:33 +0900

    :lipstick: Transparent native macOS title bar

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,13 +10,56 @@ use std::io::BufReader;
 use std::path::Path;
 use std::thread::sleep;
 use std::time::Duration;
-use tauri::Manager;
+use tauri::{Manager, Runtime, Window};
 use player::Player;
 use std::sync::Mutex;
 use tauri::State;
 use anyhow::Result;
+use cocoa::appkit::{NSWindow, NSWindowStyleMask, NSWindowTitleVisibility};
 
 struct PlayerState(Mutex<Player>);
+
+pub trait WindowExt {
+    #[cfg(target_os = "macos")]
+    fn set_transparent_titlebar(&self, title_transparent: bool, remove_toolbar: bool);
+}
+
+impl<R: Runtime> WindowExt for Window<R> {
+    #[cfg(target_os = "macos")]
+    fn set_transparent_titlebar(&self, title_transparent: bool, remove_tool_bar: bool) {
+        unsafe {
+            let id = self.ns_window().unwrap() as cocoa::base::id;
+            NSWindow::setTitlebarAppearsTransparent_(id, cocoa::base::YES);
+            let mut style_mask = id.styleMask();
+            style_mask.set(
+                NSWindowStyleMask::NSFullSizeContentViewWindowMask,
+                title_transparent,
+            );
+
+            if remove_tool_bar {
+                style_mask.remove(
+                    NSWindowStyleMask::NSClosableWindowMask
+                        | NSWindowStyleMask::NSMiniaturizableWindowMask
+                        | NSWindowStyleMask::NSResizableWindowMask,
+                );
+            }
+
+            id.setStyleMask_(style_mask);
+
+            id.setTitleVisibility_(if title_transparent {
+                NSWindowTitleVisibility::NSWindowTitleHidden
+            } else {
+                NSWindowTitleVisibility::NSWindowTitleVisible
+            });
+
+            id.setTitlebarAppearsTransparent_(if title_transparent {
+                cocoa::base::YES
+            } else {
+                cocoa::base::NO
+            });
+        }
+    }
+}
 
 #[tauri::command]
 fn play(path: &str, player: State<PlayerState>) -> Result<(), String> {
@@ -52,6 +95,11 @@ fn get_progress(player: State<PlayerState>) -> (f64, i64, i64) {
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let main_window = app.get_window("main").unwrap();
+            main_window.set_transparent_titlebar(true, false);
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![
             play, pause, resume, is_paused, seek_to, get_progress
         ])
--- src/App.tsx
+++ src/App.tsx
@@ -127,67 +127,70 @@ function App() {
   }, [progress])
 
   return (
-    <div className="flex flex-col h-screen overflow-hidden">
-      <div className=" flex-1 flex justify-center items-center">
-        <div style={{ width: coverSize, height: coverSize }} className="p-8">
-          <div className="h-full bg-gray-800 flex justify-center items-center">
-            <IoMusicalNotesSharp
-              className="text-gray-500 -translate-x-[5%] hover:cursor-grab"
-              size="75%"
-              onClick={openDialog}
-            />
+    <div className="relative">
+      <div data-tauri-drag-region className="absolute w-full h-7" />
+      <div className="flex flex-col h-screen overflow-hidden">
+        <div className=" flex-1 flex justify-center items-center">
+          <div style={{ width: coverSize, height: coverSize }} className="p-8 mt-2">
+            <div className="h-full bg-gray-800 flex justify-center items-center">
+              <IoMusicalNotesSharp
+                className="text-gray-500 -translate-x-[5%] hover:cursor-grab"
+                size="75%"
+                onClick={openDialog}
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <div ref={divExcludingCoverRef}>
-        <div className="flex justify-center items-center">
-          <div className="break-all mx-8 font-bold flex text-3xl text-gray-100 justify-center items-center">
-            MONTERO (Call Me By Your Name)
+        <div ref={divExcludingCoverRef}>
+          <div className="flex justify-center items-center">
+            <div className="break-all mx-8 font-bold flex text-3xl text-gray-100 justify-center items-center">
+              MONTERO (Call Me By Your Name)
+            </div>
           </div>
-        </div>
-        <div className="break-all mx-8 mb-4 flex text-lg text-gray-500 justify-center items-center">
-          Lil Nas X
-        </div>
-        <div className="mx-8">
-          <div className="group relative w-full ">
-            <div
-                ref={divProgressBarRef}
-                className="py-2"
-                onMouseDown={handleMouseDownProgressBar}
+          <div className="break-all mx-8 mb-4 flex text-lg text-gray-500 justify-center items-center">
+            Lil Nas X
+          </div>
+          <div className="mx-8">
+            <div className="group relative w-full ">
+              <div
+                  ref={divProgressBarRef}
+                  className="py-2"
+                  onMouseDown={handleMouseDownProgressBar}
+              >
+                <div className="rounded-full w-full h-1 bg-gray-500 absolute top-0 bottom-0 left-0 right-0 m-auto" />
+              </div>
+              <div style={{width: `${progress[0]}%`}} className={`rounded-full h-1 bg-gray-300 group-hover:bg-cyan-500 absolute top-0 bottom-0 left-0 my-auto pointer-events-none ${isDraggingProgressBar && "bg-cyan-500"}`} />
+              <div style={{left: `${progress[0]}%`}} className={`rounded-full w-4 h-4 bg-gray-300 opacity-0 group-hover:opacity-100 absolute top-0 bottom-0 my-auto -translate-x-[50%] pointer-events-none ${isDraggingProgressBar && "opacity-100"}`} />
+            </div>
+            <div className="flex">
+              <div className="text-gray-500 text-sm">
+                {formatTime(progress[1])}
+              </div>
+              <div className="text-gray-500 text-sm ml-auto">
+                {formatTime(progress[2])}
+              </div>
+            </div>
+          </div>
+          <div className="p-8 pt-4 flex justify-center items-center">
+            <button className="cursor-default text-4xl text-gray-300 hover:text-gray-50 hover:scale-105 active:text-gray-300 active:scale-100">
+              <IoPlayBackSharp />
+            </button>
+            <button
+                className="mx-16 text-5xl translate-x-1 cursor-default text-gray-300 hover:text-white hover:scale-105 active:text-gray-300 active:scale-100"
+                onClick={() => {
+                  if (isPaused) {
+                    invoke("resume")
+                  } else {
+                    invoke("pause")
+                  }
+                }}
             >
-              <div className="rounded-full w-full h-1 bg-gray-500 absolute top-0 bottom-0 left-0 right-0 m-auto" />
-            </div>
-            <div style={{width: `${progress[0]}%`}} className={`rounded-full h-1 bg-gray-300 group-hover:bg-cyan-500 absolute top-0 bottom-0 left-0 my-auto pointer-events-none ${isDraggingProgressBar && "bg-cyan-500"}`} />
-            <div style={{left: `${progress[0]}%`}} className={`rounded-full w-4 h-4 bg-gray-300 opacity-0 group-hover:opacity-100 absolute top-0 bottom-0 my-auto -translate-x-[50%] pointer-events-none ${isDraggingProgressBar && "opacity-100"}`} />
+              {isPaused ? <IoPlaySharp /> : <IoPauseSharp />}
+            </button>
+            <button className="cursor-default text-4xl text-gray-300 hover:text-white hover:scale-105 active:text-gray-300 active:scale-100">
+              <IoPlayForwardSharp />
+            </button>
           </div>
-          <div className="flex">
-            <div className="text-gray-500 text-sm">
-              {formatTime(progress[1])}
-            </div>
-            <div className="text-gray-500 text-sm ml-auto">
-              {formatTime(progress[2])}
-            </div>
-          </div>
-        </div>
-        <div className="p-8 pt-4 flex justify-center items-center">
-          <button className="cursor-default text-4xl text-gray-300 hover:text-gray-50 hover:scale-105 active:text-gray-300 active:scale-100">
-            <IoPlayBackSharp />
-          </button>
-          <button
-              className="mx-16 text-5xl translate-x-1 cursor-default text-gray-300 hover:text-white hover:scale-105 active:text-gray-300 active:scale-100"
-              onClick={() => {
-                if (isPaused) {
-                  invoke("resume")
-                } else {
-                  invoke("pause")
-                }
-              }}
-          >
-            {isPaused ? <IoPlaySharp /> : <IoPauseSharp />}
-          </button>
-          <button className="cursor-default text-4xl text-gray-300 hover:text-white hover:scale-105 active:text-gray-300 active:scale-100">
-            <IoPlayForwardSharp />
-          </button>
         </div>
       </div>
     </div>
